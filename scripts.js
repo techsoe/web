ea0282019e543323a7cb9c7fa97a05f3eae1b2ee
John David <david.ojascastro@whitecloak.com>
2023-09-28 16:57:19 +0800

    added alert dialog in button submit

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 
 var headerName = document.getElementById("headerName");
 
-
-
 headerName.onclick = function(){
 
     window.setInterval(function(){
@@ -24,5 +22,17 @@ function dateFixer(num){
         return newHour = "0" + num
     }else {
         return newHour = num.toString();
+    }
+}
+
+function showAlert(){
+    var guestNameElements = document.getElementsByClassName("name");
+
+    if(guestNameElements.length > 0){
+        var finalName = guestNameElements[0].value;
+        alert(
+            "Hello " + finalName.toUpperCase() + 
+            "\n If you want to know the time click TECHSOE"
+        );
     }
 }
